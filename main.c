1ee35d9f32be33182e4e3a5010e6c196bb11acf8
Felipe <felipenegrelleramos@gmail.com>
2023-03-09 15:58:35 -0300

    alteracoes 09/03

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
+<<<<<<< HEAD
 #include <stdio.h>
 
 int main() {
     printf("Hello World!");
     return 0;
 }
+=======
+# include <stdio.h>
+# include <stdlib.h>
+
+void basicCalc() {
+    int num1;
+    int num2;
+    printf("Basic integer sum calculator\n");
+    
+    printf("Type first number: ");
+    scanf("%d", &num1);
+    
+    printf("Type second number: ");
+    scanf("%d", &num2);
+    
+    printf("the sum is: %d", num1 + num2);
+}
+
+
+
+int main() {
+    basicCalc();
+    return 0;
+}
+
+>>>>>>> 6b96533 (alteracoes 09/03)
