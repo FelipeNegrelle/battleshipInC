8e56b08246a725d2257000d2bafcd6e0219c3189
Felipe <felipenegrelleramos@gmail.com>
2023-03-25 16:15:23 -0300

    25/3

--- battleship.c
+++ battleship.c
@@ -24,12 +24,8 @@ VERMELHO_CLARO = 12,
 ROXO_CLARO = 13,
 AMARELO = 14
 */
-void sobre() {
-	c_textcolor(14);
-	printf("Projeto desenvolvido por Felipe Negrelle\n");
-	printf("Na matéria de projeto integrador I\n");
-	printf("Github: https://github.com/felipenegrelle\n");
-}
+void sobre();
+void regras();
 
 int selecionaDificuldade() {
 	printf("Selecione a dificuldade do jogo:\n");
@@ -46,7 +42,7 @@ int selecionaDificuldade() {
 
 void criaTabuleiro(int dificuldade) {
     int linha, coluna;
-
+	int tabuleiro[dificuldade][dificuldade];
     for(linha = 0; linha < 5 ; linha++)
         for(coluna=0; coluna < 5; coluna++)
             tabuleiro[linha][coluna]=-1;
@@ -62,7 +58,6 @@ void novoJogo() {
 	switch(dificuldade) {
 		case 1: 
 			tamanhoTabuleiro = FACIL;
-			int tabuleiro[][tamanhoTabuleiro];
 			break;
 		case 2: 
 			tamanhoTabuleiro = MEDIO;
@@ -77,9 +72,9 @@ void novoJogo() {
 
 void telaInicial() {
 	int opcao;
-	c_textcolor(14);
+	textcolor(14);
 	printf("Bem-vindo ao jogo Batalha-Naval:\n");
-	c_textcolor(9);
+	textcolor(9);
 	printf("1. Novo Jogo\n");
 	printf("2. Regras\n");
 	printf("3. Sobre\n");
@@ -92,6 +87,7 @@ void telaInicial() {
 			break;
 		
 		case 2:
+			regras();
 			break;
 		
 		case 3:
@@ -103,3 +99,35 @@ void telaInicial() {
 	}
 }
 
+void sobre() {
+	textcolor(14);
+	printf("Projeto desenvolvido por Felipe Negrelle\n");
+	printf("Na matéria de projeto integrador I\n");
+	printf("Github: https://github.com/felipenegrelle\n");
+	printf("Aperte qualquer tecla para voltar ao menu inicial\n");
+	getch();
+	telaInicial();
+}
+
+void regras() {
+	textcolor(14);
+	printf("O jogo consiste em acertar todos os navios do oponente.\n");
+	printf("O tabuleiro é composto por 10x10 posições.\n");
+	printf("Os navios são compostos por 3 partes.\n");
+	printf("Os barcos que deverão ser acertados serão:\n");
+	printf("1. Porta-aviões: 3 partes\n");
+	printf("2. Encouraçado: 2 partes\n");
+	printf("3. Submarino: 1 parte\n");
+	printf("\n");
+	printf("O jogador tem 3 vidas.\n");
+	printf("Cada vez que o jogador acertar um navio, ele ganha um ponto.\n");
+	printf("\n");
+	printf("O jogador pode escolher entre 3 dificuldades:\n");
+	printf("1. Fácil: tabuleiro 10x10\n");
+	printf("2. Médio: tabuleiro 15x15\n");
+	printf("3. Difícil: tabuleiro 20x20\n");
+	printf("Bom jogo\n");
+	printf("Aperte qualquer tecla para voltar ao menu inicial\n");
+	getch();
+	telaInicial();
+}
---
+++ colors.h
@@ -0,0 +1,4 @@
+COLORS {
+    AMARELO = "\e",
+    
+}
