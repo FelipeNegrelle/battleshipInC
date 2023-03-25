a5a8614f60228e0d5c2fadf70ef91d4d0374f37f
felipe negrelle <felipenegrelleramos@gmail.com>
2023-03-25 13:11:43 -0300

    aula 24/3

--- battleship.c
+++ battleship.c
@@ -7,13 +7,25 @@
 #define FACIL 10
 #define	MEDIO 15
 #define DIFICIL 20
-
-void colorir(char cor[]) {
-	c_textcolor(3);
-	printf("%s", cor);
-}
-
+/*
+PRETO = 0,
+AZUL = 1,
+VERDE = 2,
+CIANO = 3,
+VERMELHO = 4,
+ROXO = 5,
+MARROM = 6,
+CINZA_CLARO = 7,
+CINZA_ESCURO = 8,
+AZUL_CLARO = 9,
+VERDE_CLARO = 10,
+CIANO_CLARO = 11,
+VERMELHO_CLARO = 12,
+ROXO_CLARO = 13,
+AMARELO = 14
+*/
 void sobre() {
+	c_textcolor(14);
 	printf("Projeto desenvolvido por Felipe Negrelle\n");
 	printf("Na matéria de projeto integrador I\n");
 	printf("Github: https://github.com/felipenegrelle\n");
@@ -32,15 +44,31 @@ int selecionaDificuldade() {
 
 /**/
 
-void criaTabuleiro() {
+void criaTabuleiro(int dificuldade) {
+    int linha, coluna;
+
+    for(linha = 0; linha < 5 ; linha++)
+        for(coluna=0; coluna < 5; coluna++)
+            tabuleiro[linha][coluna]=-1;
+}
+
+void mostraTabuleiro() {
 }
 
 void novoJogo() {
+	int tamanhoTabuleiro;
 	int dificuldade = selecionaDificuldade();
-	
+
 	switch(dificuldade) {
 		case 1: 
-			printf("deu merda");
+			tamanhoTabuleiro = FACIL;
+			int tabuleiro[][tamanhoTabuleiro];
+			break;
+		case 2: 
+			tamanhoTabuleiro = MEDIO;
+			break;
+		case 3: 
+			tamanhoTabuleiro = DIFICIL;
 			break;
 		default:
 			break;
@@ -49,13 +77,12 @@ void novoJogo() {
 
 void telaInicial() {
 	int opcao;
+	c_textcolor(14);
 	printf("Bem-vindo ao jogo Batalha-Naval:\n");
+	c_textcolor(9);
 	printf("1. Novo Jogo\n");
-	printf("2. Continuar Jogo\n");
-	printf("3. Cadastrar\n");
-	printf("4. Conectar-se\n");
-	printf("5. Regras\n");
-	printf("6. Sobre\n");
+	printf("2. Regras\n");
+	printf("3. Sobre\n");
 
 	scanf("%d", &opcao);
 	
@@ -68,21 +95,8 @@ void telaInicial() {
 			break;
 		
 		case 3:
-			colorir("amarelo");
-			break;
-		
-		case 4:
-
-			break;
-		
-		case 5:
-
-			break;
-		
-		case 6:
 			sobre();
 			break;
-		
 		default:
 			printf("default");
 			break;
