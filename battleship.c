05e1c5ac932fb753360b7601c87e01e85948e968
felipe negrelle <felipenegrelleramos@gmail.com>
2023-03-26 21:03:19 -0300

    26/03

--- battleship.c
+++ battleship.c
@@ -26,6 +26,7 @@ AMARELO = 14
 */
 void sobre();
 void regras();
+void telaInicial();
 
 int selecionaDificuldade() {
 	printf("Selecione a dificuldade do jogo:\n");
@@ -70,11 +71,46 @@ void novoJogo() {
 	}
 }
 
+void sobre() {
+	c_textcolor(14);
+	printf("Projeto desenvolvido por Felipe Negrelle\n");
+	printf("Na matéria de projeto integrador I\n");
+	printf("Github: https://github.com/felipenegrelle\n");
+	printf("Aperte qualquer tecla para voltar ao menu inicial\n");
+	c_getch();
+	telaInicial();
+}
+
+void regras() {
+	c_textcolor(14);
+	printf("O jogo consiste em acertar todos os navios do oponente.\n");
+	printf("O tabuleiro é composto por 10x10 posições.\n");
+	printf("Os navios são compostos por 3 partes.\n");
+	printf("Os barcos que deverão ser acertados serão:\n");
+	printf("1. Porta-aviões: 3 partes\n");
+	printf("2. Encouraçado: 2 partes\n");
+	printf("3. Submarino: 1 parte\n");
+	printf("\n");
+	printf("O jogador tem 3 vidas.\n");
+	printf("Cada vez que o jogador acertar um navio, ele ganha um ponto.\n");
+	printf("\n");
+	printf("O jogador pode escolher entre 3 dificuldades:\n");
+	printf("1. Fácil: tabuleiro 10x10\n");
+	printf("2. Médio: tabuleiro 15x15\n");
+	printf("3. Difícil: tabuleiro 20x20\n");
+	printf("Bom jogo\n");
+	printf("Aperte qualquer tecla para voltar ao menu inicial\n");
+	system("pause");
+	// system("clear");
+	// c_getch();
+	telaInicial();
+}
+
 void telaInicial() {
 	int opcao;
-	textcolor(14);
+	c_textcolor(14);
 	printf("Bem-vindo ao jogo Batalha-Naval:\n");
-	textcolor(9);
+	c_textcolor(9);
 	printf("1. Novo Jogo\n");
 	printf("2. Regras\n");
 	printf("3. Sobre\n");
@@ -94,40 +130,7 @@ void telaInicial() {
 			sobre();
 			break;
 		default:
-			printf("default");
+			printf("Digite uma opçao valida");
 			break;
 	}
-}
-
-void sobre() {
-	textcolor(14);
-	printf("Projeto desenvolvido por Felipe Negrelle\n");
-	printf("Na matéria de projeto integrador I\n");
-	printf("Github: https://github.com/felipenegrelle\n");
-	printf("Aperte qualquer tecla para voltar ao menu inicial\n");
-	getch();
-	telaInicial();
-}
-
-void regras() {
-	textcolor(14);
-	printf("O jogo consiste em acertar todos os navios do oponente.\n");
-	printf("O tabuleiro é composto por 10x10 posições.\n");
-	printf("Os navios são compostos por 3 partes.\n");
-	printf("Os barcos que deverão ser acertados serão:\n");
-	printf("1. Porta-aviões: 3 partes\n");
-	printf("2. Encouraçado: 2 partes\n");
-	printf("3. Submarino: 1 parte\n");
-	printf("\n");
-	printf("O jogador tem 3 vidas.\n");
-	printf("Cada vez que o jogador acertar um navio, ele ganha um ponto.\n");
-	printf("\n");
-	printf("O jogador pode escolher entre 3 dificuldades:\n");
-	printf("1. Fácil: tabuleiro 10x10\n");
-	printf("2. Médio: tabuleiro 15x15\n");
-	printf("3. Difícil: tabuleiro 20x20\n");
-	printf("Bom jogo\n");
-	printf("Aperte qualquer tecla para voltar ao menu inicial\n");
-	getch();
-	telaInicial();
 }
